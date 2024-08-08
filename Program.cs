84134844df783a0914a71967983c058c24147aee
saher000 <saher.tota@gmail.com>
2024-08-08 17:38:21 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+public class Program{
+ public static void Main()
+ {
+    Console.ForegroundColor=ConsoleColor.Green;
+    
+
+   var table = new Table();
+
+        // Add some columns
+        table.AddColumn("Foo");
+        table.AddColumn("Bar");
+
+        // Add some rows
+        table.AddRow("Baz", "Qux");
+        table.AddRow("Corge", "Grault");
+        table.AddRow("saher","ibrahim");
+    table.BorderColor(Color.Aqua);
+
+    AnsiConsole.Write(table);
+
+
+ } 
+}
